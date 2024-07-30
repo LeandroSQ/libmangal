c177eac06a27184546abac50e2811878508a3cb8
David Luevano Alvarado <david@luevano.xyz>
2024-07-30 12:30:24 -0600

    chore: remove leftover errors

--- metadata/anilist/error.go
+++ metadata/anilist/error.go
@@ -1,29 +1,8 @@
 package anilist
 
+// Error is a general error for Anilist operations.
 type Error string
 
 func (e Error) Error() string {
 	return "anilist: " + string(e)
 }
-
-// OAuthError is used for generic Anilist OAuth errors.
-type OAuthError string
-
-func (e OAuthError) Error() string {
-	return "anilist oauth: " + string(e)
-}
-
-type OAuthConvertCodeError string
-
-func (e OAuthConvertCodeError) Error() string {
-	return "anilist oauth converting access code to access token: " + string(e)
-}
-
-// Some specific errors
-
-var (
-	OAuthErrEmptyClientID    = OAuthError("ClientID is empty")
-	OAuthErrEmptyCode        = OAuthError("Access Code/Token is empty")
-	OAuthErrNilToken         = OAuthError("Token is nil")
-	OAuthErrTooManyCallbacks = OAuthError("Too many callback requests")
-)
--- metadata/error.go
+++ metadata/error.go
@@ -6,15 +6,3 @@ type Error string
 func (e Error) Error() string {
 	return "metadata: " + string(e)
 }
-
-type AuthError string
-
-func (e AuthError) Error() string {
-	return "auth: " + string(e)
-}
-
-type CodeGrantError string
-
-func (e CodeGrantError) Error() string {
-	return "code grant: " + string(e)
-}
